d855763f528ebabb5d8d60121709d159d4b0d871
Mann275 <patelmann2705@gmail.com>
2026-01-22 09:50:37 +0530

    counter project

--- React/02Counter/src/App.jsx
+++ React/02Counter/src/App.jsx
@@ -3,30 +3,26 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-
-  let [counter,setCounter]= useState(0)
-
+  let [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    if(counter<5)
-{    counter += 1;
-    // setCounter(counter + 1);
-    setCounter(counter);
-    console.log("Add button clicked", counter);}
-    else(console.log("Counter cannot exceed 5"))
-
+    if (counter < 5) {
+      counter += 1;
+      // setCounter(counter + 1);
+      setCounter(counter);
+      console.log("Add button clicked", counter);
+    } else console.log("Counter cannot exceed 5");
   };
 
   const removeValue = () => {
-    if(counter>0)
-{    counter -= 1;
-    setCounter(counter);
-    console.log("Remove button clicked", counter);}
-    else(console.log("Counter cannot be less than 0"))
-
+    if (counter > 0) {
+      counter -= 1;
+      setCounter(counter);
+      console.log("Remove button clicked", counter);
+    } else console.log("Counter cannot be less than 0");
   };
 
-  return (  
+  return (
     <>
       <h1>Counter</h1>
       <h3>Counter Value: {counter}</h3>
